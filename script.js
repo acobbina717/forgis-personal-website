955d31be071c9542886eb8b46e34c9f94849d7d1
Abeku <acobbina717@gmail.com>
2022-03-12 01:41:01 -0500

    website with html-ccs-javascript

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const input = document.getElementById("addedPlanets");
+const ul = document.getElementById("planetsList");
 
-document.querySelector("body").appendChild(h2);
+function addPlanetToList() {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li)
+    input.value = "";
+
+}
+
+function inputLength() {
+    return input.value.length;
+}
+function addPlanetAfterKeydown(event) {
+    if (inputLength() > 0 && event.which === 13) {
+        addPlanetToList();
+    }
+}
+
+input.addEventListener("keydown", addPlanetAfterKeydown);
+
+
+const submitPlanetButton = document.getElementById("planetSubmit")
+
+submitPlanetButton.addEventListener("click", addPlanetToList);
+
